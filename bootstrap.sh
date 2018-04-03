993903070ed8db38dee70a3c6072bc9219a756bc
Taylor Brooks <gtbiscool@gmail.com>
2018-04-03 11:52:42 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,13 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
+	
 	source ~/.bash_profile;
 }
 
